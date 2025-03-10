6c0df1182a33cbef53c41340440ee76064aeee3d
pengmao <pengmao@yunqilaohe.com>
2025-03-10 18:09:11 +0800

    feat: two-1

--- src/generate.py
+++ src/generate.py
@@ -30,37 +30,27 @@ class HoroscopeGenerator:
         self.max_length = config.max_length  # 使用配置文件中的最大长度
 
     def generate(self, seed_text, num_words=50, temperature=0.9):
-        # 将种子文本分词并转换为数字序列
         seed_tokens = jieba.cut(seed_text)
         seed_sequence = self.tokenizer.texts_to_sequences([" ".join(seed_tokens)])[0]
         
-        # 开始生成文本
         for i in range(num_words):
-            # 填充序列，确保输入长度为 max_length
             padded_seq = pad_sequences([seed_sequence], maxlen=self.max_length, padding="post")
-            
-            # 预测结果 shape: (1, max_length, vocab_size)
             predictions = self.model.predict(padded_seq)
-            # 选择最后一个时间步的预测结果，得到1D数组，长度为 vocab_size
             predicted_probs = predictions[0][-1]
             
-            # 如果存在 <OOV> 标记，将其概率设置为0
             oov_index = self.tokenizer.word_index.get("<OOV>")
             if oov_index is not None and oov_index < len(predicted_probs):
-                predicted_probs[oov_index] = 0
+                predicted_probs[oov_index] = 0  # 将 <OOV> 的概率设置为0
             
-            # 温度采样生成下一个词的索引
             predicted_id = sample_with_temperature(predicted_probs, temperature)
             seed_sequence.append(predicted_id)
             
-            # 如果生成的单词为句号、感叹号或问号，则停止生成
-            generated_word = self.tokenizer.sequences_to_texts([[predicted_id]])[0]
-            if generated_word in ['。', '！', '？']:
+            # 检查是否生成了过多的 <OOV>，如果是，则提前停止
+            if seed_sequence.count(self.tokenizer.word_index.get("<OOV>")) > 5:
                 break
-        
-        # 将生成的词序列转换为文本并返回
+            
         generated_text = self.tokenizer.sequences_to_texts([seed_sequence])[0]
-        return generated_text.replace(" ", "")  # 移除空格
+        return generated_text.replace(" ", "")
 
 if __name__ == "__main__":
     # 构造 tokenizer 的绝对路径
